d6944a265d35a7967181688681b7e686a1f76706
Ranim Manai <ranimmanai123@gmail.com>
2025-06-22 17:16:51 +0100

    mobile1

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -84,6 +84,7 @@ async function bootstrap() {
       'http://192.168.1.44:3000',
       'http://192.168.0.44:3000',
       'http://127.0.0.1:8081',
+      'http://localhost:8081',
     ];
 
     const isAllowedOrigin = (origin: string) => {
